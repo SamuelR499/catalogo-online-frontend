3a473222bbc64adbb5c04fd1ce79eacd0d4e6908
SamuelR499 <samuelribeiro2@hotmail.com>
2024-09-23 14:57:36 -0300

    mudança de produtos e informações

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Carousel from '../components/Carousel';
 export default function Home() {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-center text-2xl font-bold my-4">Catálogo de Camisetas</h1>
+      <h1 className="text-center text-2xl font-bold my-4">Catálogo de Produtos</h1>
       <Carousel />
     </div>
   );
